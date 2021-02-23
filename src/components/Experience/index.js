1f793320c5580f86e4a51944f715f17275018ee2
paulosilva <paulosilva@chinasystems.com>
2021-02-23 17:00:30 +0000

    reformat experience and education cards

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -67,11 +67,8 @@ const Experience = () => {
                     JOB POSITION
                   </Typography>
                   <Typography align="justify">
-                    I’m a paragraph. Click here to add your own text and edit
-                    me. It’s easy. Just click “Edit Text” or double click me to
-                    add your own content and make changes to the font. I’m a
-                    great place for you to tell a story and let your users know
-                    a little more about you.
+                    This is a great place for you to tell a story and let your
+                    users know a little more about you.
                   </Typography>
                 </Paper>
               </TimelineContent>
@@ -88,11 +85,8 @@ const Experience = () => {
                     JOB POSITION
                   </Typography>
                   <Typography align="justify">
-                    I’m a paragraph. Click here to add your own text and edit
-                    me. It’s easy. Just click “Edit Text” or double click me to
-                    add your own content and make changes to the font. I’m a
-                    great place for you to tell a story and let your users know
-                    a little more about you.
+                    This is a great place for you to tell a story and let your
+                    users know a little more about you.
                   </Typography>
                 </Paper>
               </TimelineContent>
@@ -109,11 +103,8 @@ const Experience = () => {
                     JOB POSITION
                   </Typography>
                   <Typography align="justify">
-                    I’m a paragraph. Click here to add your own text and edit
-                    me. It’s easy. Just click “Edit Text” or double click me to
-                    add your own content and make changes to the font. I’m a
-                    great place for you to tell a story and let your users know
-                    a little more about you.
+                    This is a great place for you to tell a story and let your
+                    users know a little more about you.
                   </Typography>
                 </Paper>
               </TimelineContent>
